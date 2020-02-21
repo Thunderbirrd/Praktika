96ad77a604714f7db4ed4c9abd441eafd4a77f85
Artem <artemis.gorb.14@gmail.com>
2020-02-21 20:41:24 +0300

    Laba № 1

---
+++ src/com/company/Laba1.java
@@ -0,0 +1,47 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Laba1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String brackets = scanner.nextLine();
+        Stack<Character> st = new Stack<>();
+        boolean t = false;
+        for (int i = 0; i < brackets.length(); i++){
+            char c = brackets.charAt(i);
+            if(c == '[' || c == '{' || c == '('){
+                st.add(c);
+            }else if(c == '}' || c == ']' || c == ')'){
+                if(st.empty()){
+                    t = true;
+                    break;
+                }
+                switch (c){
+                    case '}':
+                        if(st.peek() == '{'){
+                            st.pop();
+                        }else {break;}
+                        break;
+                    case ']':
+                        if(st.peek() == '['){
+                            st.pop();
+                        }else {break;}
+                        break;
+                    case ')':
+                        if(st.peek() == '('){
+                            st.pop();
+                        }else {break;}
+                        break;
+                }
+            }
+        }
+
+        if(st.empty() && !t){
+            System.out.println("OK");
+        }else{
+            System.out.println("Не все скобки закрылись");
+        }
+    }
+}
